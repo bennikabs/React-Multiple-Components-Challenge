eeac716e5f7ca219e7db41016353d150ecb59f56
bennikabs <114034060+bennikabs@users.noreply.github.com>
2023-08-03 17:34:09 +0200

    linked the Header component

--- src/Cities.js
+++ src/Cities.js
@@ -1,12 +1,15 @@
 import React from "react";
-
+import Header from "./Header";
 export default function Cities() {
   return (
+    <div className="Cities">
+    <Header />
     <ul>
-      <li>Paris</li>
-      <li>Moscow</li>
-      <li>San Francisco</li>
-      <li>Sydney</li>
+        <li>Paris</li>
+        <li>Moscow</li>
+        <li>San Francisco</li>
+        <li>Sydney</li>
     </ul>
+    </div>
   );
 }
